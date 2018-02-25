21d30b2a11460d6a268ea1da7b38fcfd4fecb36c
Brooks Becton <broabect@ut.utm.edu>
2018-02-24 19:59:29 -0600

    Added Github link

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -1,4 +1,4 @@
-import { Menu } from "antd";
+import { Icon, Menu } from "antd";
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 
@@ -36,6 +36,15 @@ class TopNav extends Component {
         </Menu.Item>
         <Menu.Item key="about">
           <Link to="/about">About</Link>
+        </Menu.Item>
+        <Menu.Item style={{ float: "right" }}>
+          <a
+            href="https://github.com/brooksbecton/SWAPI-Graph"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon style={{ marginRight: 0 }} type="github" />
+          </a>
         </Menu.Item>
       </Menu>
     );
