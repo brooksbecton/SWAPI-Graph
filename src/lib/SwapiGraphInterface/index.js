61bb11c41fe47b88b173440def3887a2ae558863
Brooks Becton <broabect@ut.utm.edu>
2018-05-13 11:31:20 -0500

    Cleaned Up SWAPI Interface

--- src/lib/SwapiGraphInterface/index.js
+++ src/lib/SwapiGraphInterface/index.js
@@ -9,6 +9,56 @@ export default (url: string, data: ?{}) => {
   let nodes: Array<Object> = [];
   let edges: Array<{ from: string, to: string }> = [];
   let knownSwapiCollections: Array<String> = [];
+
+  /**
+   * Gets all items from a SWAPI collection and
+   * adds it to 'nodes' property
+   */
+  const addEdges = async () => {
+    if (knownSwapiCollections.length === 0) {
+      knownSwapiCollections = await getKnownCollection();
+    }
+
+    nodes.forEach(node => {
+      knownSwapiCollections.forEach(collectionName => {
+        if (node.hasOwnProperty(collectionName)) {
+          node[collectionName].forEach(url => {
+            edges = edges.concat({ from: node.url, to: url });
+          });
+        }
+      });
+    });
+    return edges;
+  };
+
+  const getCollectionItems = async (collectionName: string) => {
+    let targetUrl: string;
+    if (collectionName) {
+      targetUrl = swapiBaseUrl + collectionName;
+    } else {
+      targetUrl = url;
+    }
+    while (targetUrl !== null) {
+      const resp = await fetch(targetUrl);
+      const { next, results } = await resp.json();
+      // Assigning name so that graph can display something
+      results.forEach(item => (item.label = item.name || item.title));
+
+      // Grouping nodes by their collection
+      results.forEach(item => {
+        item.id = item.url;
+        item.group = item.url.slice(7, item.url.length).split("/")[3];
+      });
+
+      // Targeting next page of results
+      targetUrl = next;
+
+      // Saving current results
+      nodes = nodes.concat(results);
+    }
+    return nodes;
+  };
+
   /**
    * Queries the SWAPI for it's known collection like people, films, etc
    * and adds it the object prop 'knownSwapiCollections'
@@ -40,57 +90,9 @@ export default (url: string, data: ?{}) => {
   };
 
   return {
-    /**
-     * Gets all items from a SWAPI collection and
-     * adds it to 'nodes' property
-     */
-    getCollectionItems: async (collectionName: string) => {
-      let targetUrl: string;
-      if (collectionName) {
-        targetUrl = swapiBaseUrl + collectionName;
-      } else {
-        targetUrl = url;
-      }
-      while (targetUrl !== null) {
-        const resp = await fetch(targetUrl);
-        const { next, results } = await resp.json();
-        // Assigning name so that graph can display something
-        results.forEach(item => (item.label = item.name || item.title));
-
-        // Grouping nodes by their collection
-        results.forEach(item => {
-          item.id = item.url;
-          item.group = item.url.slice(7, item.url.length).split("/")[3];
-        });
-
-        // Targeting next page of results
-        targetUrl = next;
-
-        // Saving current results
-        nodes = nodes.concat(results);
-      }
-      return nodes;
-    },
-
-    getKnownCollection: async () => await getKnownCollection(),
-
-    addEdges: async () => {
-      if (knownSwapiCollections.length === 0) {
-        knownSwapiCollections = await getKnownCollection();
-      }
-
-      nodes.forEach(node => {
-        knownSwapiCollections.forEach(collectionName => {
-          if (node.hasOwnProperty(collectionName)) {
-            node[collectionName].forEach(url => {
-              edges = edges.concat({ from: node.url, to: url });
-            });
-          }
-        });
-      });
-      return edges;
-    },
-
-    getNodesEdges: getNodesEdges
+    addEdges,
+    getCollectionItems,
+    getKnownCollection,
+    getNodesEdges
   };
 };
